8af424010ef0aca130f7f07fd787c1cdf71936ce
bmurdock <brian.j.murdock@gmail.com>
2020-04-23 19:41:21 -0600

    First commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const port = 8765;
+
+const users = [{
+    name: 'Brian Murdock',
+    email: 'brian@helio.com',
+    id: 312,
+}, {
+    name: 'Alex Something',
+    email: 'alex@helio.com',
+    id: 313,
+}, {
+    name: 'Chris Smith',
+    email: 'csmith@helio.com',
+    id: 314,
+}, {
+    name: 'Giacinta Bianchi',
+    email: 'giabia@helio.com',
+    id: 315,
+}];
+
+const app = express();
+
+//app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+// create a route for the index page
+app.get('/', (req, res, next) => {
+    res.render('pages/index', {
+        color: 'red',
+        title: 'Welcome',
+        users
+    });
+});
+
+const profileLookup = (_id) => {
+    /* 
+        This function should return an array of matching users
+        Hopefully that array only has 1 element which is THE
+        matching user. So the result should look like:
+        [{
+            name: 'Brian Murdock',
+            email: 'brian@helio.com',
+            id: 312,
+        }]
+    */
+    const id = parseInt(_id);
+    return users.filter((user) => {
+        return user.id === id;
+    });
+};
+
+app.get('/profile/:id', (req, res, next) => {
+    const id = parseInt(req.params.id);
+    let user = profileLookup(id);
+    if (user.length === 0) {
+        console.log('user not found..');
+        res.render('pages/notfound');
+        return;
+    }
+    user = user[0];
+
+    console.log('found user: ', user);
+    res.render('pages/profile', {
+        title: `Profile > ${user.name}`,
+        user,
+    });
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log('Error launching server: ', err);
+    }
+    console.log(`Application is listening on port ${port}...`);
+});
